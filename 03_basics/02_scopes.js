942eb16b54ccd17819adbe9b105087f63d533f7b
bhaktisharma07 <bhaktisharma0727@gmail.com>
2026-07-28 19:07:05 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,16 +1,80 @@
-// var c = 30 // var has global/function scope, avoid using it
+// var c = 30 // var is function scoped, avoid using it
 
-let a = 300 // Outer (global) variable
+let a = 300 // Outer variable
 
 if (true) {
-    let a = 10 // This 'a' exists only inside this block
-    const b = 20 // const is also block scoped
+    let a = 10 // Block scope
+    const b = 20
 
-    console.log("INNER :", a); // Output: 10
+    console.log("INNER :", a); // 10
 }
 
-console.log(a); // Output: 300 (Outer 'a' is not affected)
+console.log(a); // 300
 
-// console.log(b); // Error: 'b' is not accessible outside the block
+// console.log(b); // Error
+// console.log(c);
 
-// console.log(c); // If var was used, it would be accessible here because var is not block scoped
+/*
+let and const are block scoped.
+Variables inside {} cannot be accessed outside.
+Inner variable can have the same name as outer variable (Shadowing).
+*/
+
+function one() {
+    const username = "hitesh"
+
+    function two() {
+        const website = "youtube"
+        console.log(username); // Inner function can access outer variables
+    }
+
+    // console.log(website); // Error
+    two()
+}
+
+one()
+
+/*
+Child function can access parent's variables.
+Parent function cannot access child's variables.
+*/
+
+if (true) {
+    const username = "hitesh"
+
+    if (username === "hitesh") {
+        const website = "youtube"
+        console.log(username + website);
+    }
+
+    // console.log(website); // Error
+}
+
+// console.log(username); // Error
+
+
+
+// ************** Hoisting **************
+
+// Function declaration is hoisted
+console.log(addOne(5));
+
+function addOne(num) {
+    return num + 1;
+}
+
+// Function expression is NOT hoisted
+const addTwo = function(num) {
+    return num + 2;
+};
+
+console.log(addTwo(5));
+
+/*
+Function Declaration:
+- Can be called before its definition.
+
+Function Expression:
+- Cannot be called before its definition.
+- Calling it before declaration gives ReferenceError.
+*/
